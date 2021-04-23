e913175dd299b1dd8b3905a2018ed2c1f6484006
shaheen5 <miyashaheen1995@gmail.com>
2021-04-23 15:57:27 +0530

    Added : Code To Display Welcome Message

---
+++ main/java/com/linkedlist/MyNode.java
@@ -0,0 +1,7 @@
+package com.linkedlist;
+
+public class MyNode {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Linked List Implementation Problem !");
+    }
+}
